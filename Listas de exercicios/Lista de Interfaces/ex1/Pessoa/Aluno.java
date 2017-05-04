a3bc4080d6b99d964efd4dc0d4f165ea9d669da8
Bruno Pastre <pastre68@gmail.com>
2017-05-04 19:35:07 -0300

    Create Aluno.java

---
+++ Listas de exercicios/Lista de Interfaces/ex1/Pessoa/Aluno.java
@@ -0,0 +1,22 @@
+package ex1.Pessoa;
+
+public class Aluno extends Pessoa{
+
+	private String curso;
+	
+	public Aluno(String nome, String curso) {
+		super(nome);
+		this.curso = curso;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+	
+
+}
