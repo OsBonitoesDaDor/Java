fa916fe7249bedf6fa6eab2d6a7176418d60d485
Bruno Pastre <pastre68@gmail.com>
2017-06-07 02:58:55 -0300

    Create Admin.java

---
+++ Livraria Virtual/src/common/user/Admin.java
@@ -0,0 +1,12 @@
+package common.user;
+
+import common.Interfaces.IAdmin;
+//TODO : tudo
+public class Admin extends UsuarioGenerico implements IAdmin{
+
+	public Admin(String nome, String cpf, int id) {
+		super(nome, cpf, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
