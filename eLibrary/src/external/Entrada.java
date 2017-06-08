9721a61ce77d9ed3fa3414a1cd21cc5f916e295d
Bruno Pastre <pastre68@gmail.com>
2017-06-08 12:34:05 -0300

    Create Entrada.java

---
+++ eLibrary/src/external/Entrada.java
@@ -0,0 +1,58 @@
+package external;
+
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Entrada implements Serializable{
+	
+	/**
+	 * @return Proxima palavra digitada pelo usuario
+	 */
+	public String next(){
+		Scanner scan = new Scanner(System.in);
+		return scan.next();
+	}
+	
+	/**
+	 * @return Proxima linha digitada pelo usuario
+	 */
+	public String nextLine(){
+		Scanner scan = new Scanner(System.in);
+		return scan.nextLine() == "" ? "" : scan.nextLine();
+	}
+	
+	/**
+	 * @return Proximo int digitado pelo usuario
+	 */	
+	public int getInt(){
+		Scanner scan = new Scanner(System.in);
+		return scan.nextInt();
+	}
+	
+
+	/**
+	 * @return Proximo float digitado pelo usuario
+	 */	
+	public float nextFloat(){
+		Scanner scan = new Scanner(System.in);
+		return scan.nextFloat();
+	}
+	
+
+	/**
+	 * @return Proximo double digitado pelo usuario
+	 */	
+	public double nextDouble(){
+		Scanner scan = new Scanner(System.in);
+		return scan.nextDouble();
+	}
+	
+	/**
+	 * 
+	 * @return Proximo boolean digitado
+	 */
+	public boolean nextBoolean(){
+		Scanner scan = new Scanner(System.in);
+		return scan.nextBoolean();
+	}
+}
